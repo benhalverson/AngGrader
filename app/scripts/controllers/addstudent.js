5ddc103bdee98fa3685dd8796f827294b51d55cf
Ben Halverson <benhalverson33@gmail.com>
2015-07-12 18:16:27 -0700

    setting up addstudent ctrl

--- app/scripts/controllers/addstudent.js
+++ app/scripts/controllers/addstudent.js
@@ -3,15 +3,17 @@
 /**
  * @ngdoc function
  * @name angGraderApp.controller:AddstudentCtrl
- * @description
+ * @adding a student to the database
  * # AddstudentCtrl
  * Controller of the angGraderApp
  */
 angular.module('angGraderApp')
   .controller('AddstudentCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    this.student = [
+      {'name': 'Ben',
+      'cohort' 'June 2014'},
+      {'name': 'Christian',
+      'cohort' 'April 2015'}
+
+        ];
   });
