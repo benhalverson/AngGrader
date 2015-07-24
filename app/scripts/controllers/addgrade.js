59379adeb8d84d9b829aa4a73c38bc6a0f6956b2
Christian Richard <lifepupil@gmail.com>
2015-07-23 19:32:45 -0700

    drop down select for assignments working pt 2

--- app/scripts/controllers/addgrade.js
+++ app/scripts/controllers/addgrade.js
@@ -10,8 +10,16 @@
 angular.module('angGraderApp')
   .controller('AddgradeCtrl', function ($rootScope, $scope, Assignment) {
     console.log('inside the AddgradeCtrl');
+    // $scope.whichPost = {};
+    // $scope.whichStudent = {};
 
     $scope.posts = Assignment.all;
     console.log('$scope.posts', $scope.posts[0]);
 
+    $scope.gradeThis = function(grading){
+      console.log('grading', grading);
+      console.log('scope grading', $scope.grading);
+      // console.log('whichPost', $scope.whichPost);
+      // console.log('whichStudent', $scope.whichStudent);
+    }
   });
